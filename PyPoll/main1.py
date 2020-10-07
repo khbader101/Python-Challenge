c7285858160faca68b6f057e90c8dc8fe133a4d6
khbader101 <69255197+khbader101@users.noreply.github.com>
2020-10-07 16:46:15 -0400

    changed the way I count the vote back to the original way because I found an easier way to print out the name of the winner.

--- PyPoll/main1.py
+++ PyPoll/main1.py
@@ -3,7 +3,7 @@ import csv
 
 election_csv = os.path.join("Resources", "election_data.csv")
 
-total_votes = []
+total_votes = 0
 Khan = 0
 Correy = 0
 Li = 0
@@ -14,7 +14,7 @@ with open(election_csv) as csv_file:
     header = next(csvreader)
 
     for row in csvreader:
-        total_votes.append(row)
+        total_votes = total_votes + 1
 
         if row[2] == "Khan":
             Khan = Khan + 1
@@ -25,21 +25,25 @@ with open(election_csv) as csv_file:
         elif row[2] == "O'Tooley":
             OTooley = OTooley + 1
 
-length_total_votes = len(total_votes)
-Khan_Percentage = (Khan/length_total_votes)*100
-Correy_percentage = (Correy/length_total_votes)*100
-Li_percentage = (Li/length_total_votes)*100
-OTooley_percentage = (OTooley/length_total_votes)*100
+Khan_Percentage = (Khan/total_votes)*100
+Correy_percentage = (Correy/total_votes)*100
+Li_percentage = (Li/total_votes)*100
+OTooley_percentage = (OTooley/total_votes)*100
 
-winner_Votes = max([Khan, Correy, Li, OTooley])
-index = total_votes.index(winner_Votes)
-
+if Khan > Correy and Li and OTooley:
+    Winner = "Khan"
+elif Correy > Khan and Li and OTooley:
+    Winner = "Correy"
+elif Li > Khan and Correy and OTooley:
+    Winner = "Li"
+elif OTooley > Khan and Correy and Li:
+    Winner = "O'Tooley"
 
 print("Election Results\n-------------------------")
-print(f"Total Votes: {str(length_total_votes)}\n-------------------------")
+print(f"Total Votes: {str(total_votes)}\n-------------------------")
 print(f"Khan: {Khan_Percentage:.3f}% ({Khan})")
 print(f"Corry: {Correy_percentage:.3f}% ({Correy})")
 print(f"Li: {Li_percentage:.3f}% ({Li})")
 print(f"O'Tooley: {OTooley_percentage:.3f}% ({OTooley})\n-------------------------")
-
-print(index)
+print(f"Winner: {Winner}")
+print("-------------------------")
